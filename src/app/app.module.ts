39df7f8e5d0626e55baf30818bbe94f5d8471978
Amit Gupta <amit.gupta@daffodilsw.com>
2019-07-29 15:41:37 +0530

    nc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { AuthModule } from './auth/auth.module';
-import { HomeComponent } from './home/home.component';
-import { WildcardRoutingModule } from './wildcard-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './auth/auth-interceptor';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CoreModule } from "./core/core.module";
+import { AuthModule } from "./auth/auth.module";
+import { HomeComponent } from "./home/home.component";
+import { WildcardRoutingModule } from "./wildcard-routing.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ToastrModule } from "ngx-toastr";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { AuthInterceptor } from "./auth/auth-interceptor";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     CoreModule,
@@ -27,8 +24,8 @@ import { AuthInterceptor } from './auth/auth-interceptor';
     ToastrModule.forRoot()
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi:true}
+    // {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi:true}
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,79 +1,105 @@
-import { HttpClient } from '@angular/common/http';
-import { LoginData, SignupData } from './auth.model';
-import { Subject } from 'rxjs';
-import { environment } from '../../environments/environment';
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
-import { text } from '@angular/core/src/render3';
+import { HttpClient } from "@angular/common/http";
+import { LoginData, SignupData } from "./auth.model";
+import { Subject } from "rxjs";
+import { environment } from "../../environments/environment";
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { text } from "@angular/core/src/render3";
 
 const BACKEND_URL = environment.authApi;
 
 @Injectable()
 export class AuthService {
-    private isAuthenticated = false;
-    private token: string;
-    private tokenTimer: any;
-    private authStatusListener = new Subject<boolean>();
+  private isAuthenticated = false;
+  private token: string;
+  private tokenTimer: any;
+  private authStatusListener = new Subject<boolean>();
 
-    constructor(private http:HttpClient, private router: Router, private toastr:ToastrService){}
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
-    signup(signupData: SignupData){
-        this.http.post(`${BACKEND_URL}/signup`, signupData, {responseType:'text'})
-            .subscribe((token)=>{
-                this.isAuthenticated = true;
-                this.token = token;
-                this.authStatusListener.next(true);
-                this.saveAuthData(this.token);
-                this.router.navigate(['/']);
-                this.toastr.success('Signup Successful');
-            }, error=>{
-                this.authStatusListener.next(false);
-                this.toastr.error(error.error);
-            });
-    }
+  signup(signupData: SignupData) {
+    this.http
+      .post(`${BACKEND_URL}/signup`, signupData, { responseType: "text" })
+      .subscribe(
+        token => {
+          this.isAuthenticated = true;
+          this.token = token;
+          this.authStatusListener.next(true);
+          this.saveAuthData(this.token);
+          this.router.navigate(["/"]);
+          this.toastr.success("Signup Successful");
+        },
+        error => {
+          this.authStatusListener.next(false);
+          this.toastr.error(error.error);
+        }
+      );
+  }
 
-    signin(signinData: LoginData){
-        this.http.post(`${BACKEND_URL}/signin`, signinData, {responseType:'text'})
-            .subscribe((token)=>{
-                this.isAuthenticated = true;
-                this.token = token;
-                this.authStatusListener.next(true);
-                this.saveAuthData(this.token);
-                this.router.navigate(['/']);
-            }, error=>{
-                this.authStatusListener.next(false);
-                this.toastr.error(error.error);
-            });
-    }
+  signin(signinData: LoginData) {
+    this.http
+      .post(`${BACKEND_URL}/signin`, signinData, { responseType: "text" })
+      .subscribe(
+        token => {
+          this.isAuthenticated = true;
+          this.token = token;
+          this.authStatusListener.next(true);
+          this.saveAuthData(this.token);
+          this.router.navigate(["/"]);
+          this.toastr.success("Successfully Logged In");
+        },
+        error => {
+          this.authStatusListener.next(false);
+          this.toastr.error(error.error);
+        }
+      );
+  }
 
-    private saveAuthData(token:string){
-        localStorage.setItem('token', token);
-    }
+  private saveAuthData(token: string) {
+    localStorage.setItem("token", token);
+  }
 
-    getToken(){
-        return this.token;
-    }
+  getToken() {
+    return this.token;
+  }
 
-    private clearAuthdata(){
-        localStorage.removeItem('token');
-    }
+  private clearAuthdata() {
+    localStorage.removeItem("token");
+  }
 
-    getAuthStatusListener(){
-        return this.authStatusListener.asObservable();
-    }
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
+  }
 
-    getIsAuth(){
-        return this.isAuthenticated;
-    }
+  getIsAuth() {
+    return this.isAuthenticated;
+  }
 
-    logout(){
-        this.token = null;
-        this.isAuthenticated = false;
-        this.authStatusListener.next(false);
-        this.clearAuthdata();
-        this.router.navigate(['/']);
-        this.toastr.success('Logged out successfully');
-    }
+  autoAuthUser() {
+    const token = this.getAuthData();
+    if (!token) return;
+    this.token = token;
+    this.isAuthenticated = true;
+    this.authStatusListener.next(true);
+  }
 
+  getAuthData() {
+    const token = localStorage.getItem("token");
+    if (!token) return;
+    return token;
+  }
+
+  logout() {
+    this.token = null;
+    this.isAuthenticated = false;
+    this.authStatusListener.next(false);
+    this.clearAuthdata();
+    this.router.navigate(["/"]);
+    this.toastr.success("Logged out successfully");
+  }
 }
