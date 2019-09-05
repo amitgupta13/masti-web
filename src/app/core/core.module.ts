ebbdb9a30112f81d1e1d0d1c84c38e046454a17e
Amit Gupta <amit.gupta@daffodilsw.com>
2019-09-05 18:05:17 +0530

    nc

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,21 +1,14 @@
-import { NgModule } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { AppRoutingModule } from '../app-routing.module';
-import { AuthService } from '../auth/auth.service';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { HeaderComponent } from "./header/header.component";
+import { AppRoutingModule } from "../app-routing.module";
+import { AuthService } from "../auth/auth.service";
+import { CommonModule } from "@angular/common";
+import { FooterComponent } from "./footer/footer.component";
 
 @NgModule({
-    declarations:[
-        HeaderComponent,
-    ],
-    imports: [
-        CommonModule,
-        AppRoutingModule,
-    ],
-    exports: [
-        HeaderComponent
-    ],
-    providers:[AuthService]
+  declarations: [HeaderComponent, FooterComponent],
+  imports: [CommonModule, AppRoutingModule],
+  exports: [HeaderComponent, FooterComponent],
+  providers: [AuthService]
 })
-
 export class CoreModule {}
